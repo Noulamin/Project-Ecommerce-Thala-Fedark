1fce974ecaf8b23d0a90762247a10f5d57b7507f
Boubker Ennaqar <93933750+EnnaqarBoubker@users.noreply.github.com>
2022-12-06 14:06:56 +0100

    modification bd

--- server/models/index.js
+++ server/models/index.js
@@ -12,14 +12,22 @@ const db = {}
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+
 db.UserModel = require("./UserModel")(sequelize, Sequelize)
+db.RoleModel = require("./RoleModel")(sequelize, Sequelize)
 db.CategorieModel = require('./CategorieModel')(sequelize, Sequelize)
 db.CommandModel = require('./CommandModel')(sequelize, Sequelize)
 db.CommentModel = require('./CommentModel')(sequelize, Sequelize)
 db.ProduitModel = require('./ProduitModel')(sequelize, Sequelize)
 db.AvisModel = require('./AvisModel')(sequelize, Sequelize)
 
-// db.UserModel.belongsTo(db.RoleModel);
-// db.RoleModel.hasMany(db.UserModel);
+db.ProduitModel.hasMany(db.CommentModel)
+
+db.CategorieModel.hasMany(db.ProduitModel)
+db.ProduitModel.belongsTo(db.CategorieModel)
+
+
+db.UserModel.belongsTo(db.RoleModel);
+db.RoleModel.hasMany(db.UserModel);
 
 module.exports = db
