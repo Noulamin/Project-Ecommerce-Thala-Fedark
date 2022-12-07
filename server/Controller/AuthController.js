56ffe9f0d981f79c9dba166c1b7c39dcd84cc6bb
Boubker Ennaqar <93933750+EnnaqarBoubker@users.noreply.github.com>
2022-12-07 10:33:12 +0100

    create register function with successfully creation

--- server/Controller/AuthController.js
+++ server/Controller/AuthController.js
@@ -1,4 +1,13 @@
+const db = require('../Models')
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+const asyncHandler = require('express-async-handler')
+// const sendEmail = require('../Utils/sendEmail');
+// const genToken = require('../Utils/generateToken');
+// const cookie = require('cookie-parser')
 
+
+const UserModel = db.UserModel;
 
 
 /**
@@ -16,9 +25,42 @@ const login = (req, res) => {
  * url => api/auth/register
  * access => public
  */
- const register = (req, res) => {
-    res.status(200).send('this register page')
-}
+ const register = asyncHandler( async (req, res) => {
+    const { first_Name, last_Name, email, password, password2, phone_number, city, adress } = req.body
+
+    if (!first_Name || !last_Name || !email || !password || !password2 || !phone_number || !city || !adress) {
+        res.status(400).send('please add all fields')
+    } else if (password != password2) {
+        res.status(400)
+        throw new Error('password not match')
+    }
+
+    const emailExist = await UserModel.findOne({ where: { email } })
+    console.log(emailExist);
+    // if (emailExist) {
+    //     res.status(400)
+    //     throw new Error('Opps!! Email has been already taken')
+    // }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await await bcrypt.hash(password, salt);
+
+
+    const data = {
+        first_Name,
+        last_Name,
+        email,
+        password: hashPassword,
+        password2: password,
+        phone_number,
+        city,
+        adress,
+        ValidateToken: crypto.randomBytes(64).toString('hex'),
+    }
+
+    const user = await UserModel.create(data)
+    res.send('user success')
+})
 
 
 /**
