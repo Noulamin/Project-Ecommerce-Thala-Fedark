4f065ec0a38cdf5b915ad31efe8a7edc2031e779
Nouaman <89440867+Noulamin@users.noreply.github.com>
2022-12-08 14:24:36 -0800

    Routes added

---
+++ server/Routes/CodePromosRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+
+const {AddPromoCode, UpdatePromoCode, DeletePromoCode} = require('../Controller/CodePromosController')
+
+
+router.post('/AddPromoCode', AddPromoCode)
+router.post('/UpdatePromoCode/:id',UpdatePromoCode)
+router.post('/DeletePromoCode/:id',DeletePromoCode)
+
+module.exports = router
