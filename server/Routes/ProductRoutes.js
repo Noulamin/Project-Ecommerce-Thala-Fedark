dd5f1c2cba241d64802367a1a820d8bb5d692d85
NoureddineMa <noureddinemaher13@gmail.com>
2022-12-08 16:11:15 +0100

    resolve a issue

--- server/Routes/ProductRoutes.js
+++ server/Routes/ProductRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getAll,createProduct,deleteProduct,updateProduct} = require("../controller/ProductController");
+const { getAll,createProduct,deleteProduct,updateProduct} = require("../Controller/ProductController");
 
 
 router.post("/add",createProduct );
