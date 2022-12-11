ae60d4c27f0281bdf94d3392146533ce78c9f49d
NoureddineMa <noureddinemaher13@gmail.com>
2022-12-11 17:22:52 +0100

    Add test function set Comment and Avis

--- server/Controller/AvisController.js
+++ server/Controller/AvisController.js
@@ -11,25 +11,19 @@ const asyncHandler = require('express-async-handler')
         if(!req.body){
             res.json({message: "please add all fields"})
         }
-        const idprod = req.params.produitIdProduit
-        console.log(idprod);
-        const idAvis = req.params.id_avis
-        console.log(idAvis);
  
         const avi = {
-            id_avis: idAvis,
+            id_avis: req.params.id_avis,
             avis : req.body.avis,
-            produitIdProduit : idprod,
+            produitIdProduit : req.params.produitIdProduit,
         }
         try {
             await avis.create(avi)
             return res.json({message: 'Avis created successfully , thank you !'})
             .status(200)
-            console.log('inserted');
         } catch (error) {
             res.send(error)
             .status(400)
-            console.log('not inserted');
             console.log(error);
         }
   })
