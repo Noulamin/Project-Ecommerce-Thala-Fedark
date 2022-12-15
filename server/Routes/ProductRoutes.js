d8be707420eaa12e5628f133b3f40606c4953357
NoureddineMa <noureddinemaher13@gmail.com>
2022-12-15 20:15:45 +0100

    fix probleme capital letters

--- server/Routes/ProductRoutes.js
+++ server/Routes/ProductRoutes.js
@@ -9,7 +9,7 @@ router.delete("/delete/:id_produit", deleteProduct);
 router.put("/edit/:id_produit", updateProduct);
 router.get('/product/:id',getProductsByCategorie)
 
-router.get('/search/:key', searchProduit)
+// router.get('/search/:key', searchProduit)
 
 
 
