2a0416ccd2e99277bb73d20896b5f0cc155ccd13
Nouaman <89440867+Noulamin@users.noreply.github.com>
2022-12-15 18:55:16 -0800

    Commands added

--- client/src/pages/Admin/Comments.js
+++ client/src/pages/Admin/Comments.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SideBar from '../../components/Admin/SideBar'
 
-const DashAdmin = () => {
+const Comments = () => {
   
   return (
     <>
@@ -166,4 +166,4 @@ const DashAdmin = () => {
   )
 }
 
-export default DashAdmin
+export default Comments
