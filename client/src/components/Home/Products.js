0162c11b632ca96f9c5abcd52dd1791c7bd131cf
Boubker Ennaqar <93933750+EnnaqarBoubker@users.noreply.github.com>
2022-12-20 00:13:42 +0100

    get all category in home page

--- client/src/components/Home/Products.js
+++ client/src/components/Home/Products.js
@@ -1,29 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 
 const Products = () => {
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
+
+    const url = 'http://localhost:8080/Product/getAllProduct'
+    const getAllProduct = () => {
+        try {
+            axios.get(url)
+                .then(res => {
+                    console.log(res)
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <>
+            <button onClick={getAllProduct}>get ALL</button>
             <div className="container pb-16">
                 <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">recomended for you</h2>
                 <div className="grid grid-cols-4 gap-6">
                     <div className="bg-white shadow rounded overflow-hidden group">
                         <div className="relative">
-                            <img src="assets/images/products/product1.jpg" alt="product 1" className="w-full"/>
-                                <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center 
-                    justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
-                                    <Link to="#"
-                                        className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-                                        title="view product">
-                                        <i className="fa-solid fa-magnifying-glass"></i>
-                                    </Link>
-                                    <Link to="#"
-                                        className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
-                                        title="add to wishlist">
-                                        <i className="fa-solid fa-heart"></i>
-                                    </Link>
-                                </div>
+                            <img src="assets/images/products/product1.jpg" alt="product 1" className="w-full" />
+                            <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+                                <Link to="#"
+                                    className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
+                                    title="view product">
+                                    <i className="fa-solid fa-magnifying-glass"></i>
+                                </Link>
+                                <Link to="#"
+                                    className="text-white text-lg w-9 h-8 rounded-full bg-primary flex items-center justify-center hover:bg-gray-800 transition"
+                                    title="add to wishlist">
+                                    <i className="fa-solid fa-heart"></i>
+                                </Link>
+                            </div>
                         </div>
                         <div className="pt-4 pb-3 px-4">
                             <Link to="#">
@@ -35,13 +56,13 @@ const Products = () => {
                                 <p className="text-sm text-gray-400 line-through">$55.90</p>
                             </div>
                             <div className="flex items-center">
-                                <div className="flex gap-1 text-sm text-yellow-400">
+                                {/* <div className="flex gap-1 text-sm text-yellow-400">
                                     <span><i className="fa-solid fa-star"></i></span>
                                     <span><i className="fa-solid fa-star"></i></span>
                                     <span><i className="fa-solid fa-star"></i></span>
                                     <span><i className="fa-solid fa-star"></i></span>
                                     <span><i className="fa-solid fa-star"></i></span>
-                                </div>
+                                </div> */}
                                 <div className="text-xs text-gray-500 ml-3">(150)</div>
                             </div>
                         </div>
@@ -49,7 +70,8 @@ const Products = () => {
                             className="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition">Add
                             to cart</Link>
                     </div>
-                    <div className="bg-white shadow rounded overflow-hidden group">
+
+                    {/* <div className="bg-white shadow rounded overflow-hidden group">
                         <div className="relative">
                             <img src="assets/images/products/product4.jpg" alt="product 1" className="w-full"/>
                                 <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center 
@@ -335,7 +357,7 @@ const Products = () => {
                         <Link to="#"
                             className="block w-full py-1 text-center text-white bg-primary border border-primary rounded-b hover:bg-transparent hover:text-primary transition">Add
                             to cart</Link>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
